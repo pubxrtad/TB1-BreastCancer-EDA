801071a632b5893f56167ca6cb4e284ae48aa1eb
Alissa <alisita1403@gmail.com>
2025-12-05 08:46:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import pandas as pd
 import numpy as np
 #import seaborn as sns
 #import matplotlib.pyplot as plt
-#from sklearn.metrics import (
-    accuracy_score,
-    precision_score,
-    recall_score,
-    f1_score,
-    roc_auc_score,
-    confusion_matrix,
-    RocCurveDisplay
-)
 #from sklearn.model_selection import train_test_split
 #from sklearn.ensemble import RandomForestClassifier
 #from sklearn.linear_model import LogisticRegression
